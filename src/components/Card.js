8a6c77b1eef3d6bf821140b5c9b424efdd05c7a0
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-24 16:18:16 +0300

    functionality completed

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,22 @@
 import React from 'react';
 
-function Card({ data }) {
+function Card({ data, timeframe }) {
+
+	let prevTime = "day";
+	switch (timeframe) {
+		case "daily":
+			prevTime = "day"
+			break;
+		case "weekly":
+			prevTime = "week"
+			break;
+		case "monthly":
+			prevTime = "month"
+			break;
+		default:
+			break;
+	}
+
     return (
         <div className="card" style={{backgroundColor: `${data.bgColor}`}}>
             <img src={data.icon} alt="icon" />
@@ -8,10 +24,10 @@ function Card({ data }) {
                 <p className="title">{data.title}</p>
                 <div className="timeframes">
                     <p className="current">
-                        {data.timeframes.weekly.current} hrs
+                        {data.timeframes[`${timeframe}`].current} hrs
                     </p>
                     <p className="previous">
-                        Last week - {data.timeframes.weekly.previous} hrs
+                        Last {prevTime} - {data.timeframes[`${timeframe}`].previous} hrs
                     </p>
                 </div>
             </div>
